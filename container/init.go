3a6e9e4a27918a32e45aff1bbfa1b2136995f507
summer-boythink <2071792205@qq.com>
2023-07-31 20:48:28 +0800

    Add:pivotRoot

--- container/init.go
+++ container/init.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 	"syscall"
 
@@ -18,8 +19,10 @@ func RunContainerInit() error {
 	}
 
 	// if no Mount proc,syscall.Exec will fail
-	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
-	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
+	//defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
+	//syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
+
+	setupMount()
 	path, err := exec.LookPath(cmdArray[0])
 	if err != nil {
 		log.Errorf("Exec loop path error %v", err)
@@ -33,8 +36,8 @@ func RunContainerInit() error {
 }
 
 func readUserCommand() []string {
-	pipe := os.NewFile(uintptr(3),"pipe")
-	msg,err := io.ReadAll(pipe)
+	pipe := os.NewFile(uintptr(3), "pipe")
+	msg, err := io.ReadAll(pipe)
 	if err != nil {
 		log.Errorf("init read pipe error %v", err)
 		return nil
@@ -43,3 +46,46 @@ func readUserCommand() []string {
 	return strings.Split(msgStr, " ")
 }
 
+func setupMount() {
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Errorf("Get current location error %v", err)
+		return
+	}
+	log.Infof("Current location is %s", pwd)
+	pivotRoot(pwd)
+
+	//mount proc
+	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
+	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
+
+	syscall.Mount("tmpfs", "/dev", "tmpfs", syscall.MS_NOSUID|syscall.MS_STRICTATIME, "mode=755")
+}
+
+func pivotRoot(root string) error {
+	if err := syscall.Mount(root, root, "bind", syscall.MS_BIND|syscall.MS_REC,
+		""); err != nil {
+		return fmt.Errorf("mount rootfs error: %v", err)
+	}
+
+	pivotDir := filepath.Join(root, ".pivot_root")
+	if err := os.Mkdir(pivotDir, 0777); err != nil {
+		return err
+	}
+
+	if err := syscall.PivotRoot(root, pivotDir); err != nil {
+		return fmt.Errorf("pivot_root %v", err)
+	}
+	// 修改当前的工作目录到根目录
+	if err := syscall.Chdir("/"); err != nil {
+		return fmt.Errorf("chdir / %v", err)
+	}
+
+	pivotDir = filepath.Join("/", ".pivot_root")
+	// umount rootfs/.pivot_root
+	if err := syscall.Unmount(pivotDir, syscall.MNT_DETACH); err != nil {
+		return fmt.Errorf("unmount pivot_root dir %v", err)
+	}
+	// 删除临时文件夹
+	return os.Remove(pivotDir)
+}
