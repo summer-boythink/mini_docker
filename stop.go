3e06745bbf72e9586060c20fab0803fa7090571a
summer-boythink <2071792205@qq.com>
2023-10-09 14:15:43 +0800

    Add:stop

---
+++ stop.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strconv"
+	"syscall"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/summer-boythink/mydocker/container"
+)
+
+func stopContainer(containerName string) {
+	pid, err := getContainerPidByName(containerName)
+	if err != nil {
+		log.Errorf("Get contaienr pid by name %s error %v", containerName, err)
+		return
+	}
+	pidInt, err := strconv.Atoi(pid)
+	if err != nil {
+		log.Errorf("Conver pid from string to int error %v", err)
+		return
+	}
+	if err := syscall.Kill(pidInt, syscall.SIGTERM); err != nil {
+		log.Errorf("Stop container %s error %v", containerName, err)
+		return
+	}
+	containerInfo, err := getContainerInfoByName(containerName)
+	if err != nil {
+		log.Errorf("Get container %s info error %v", containerName, err)
+		return
+	}
+	containerInfo.Status = container.STOP
+	containerInfo.Pid = " "
+	newContentBytes, err := json.Marshal(containerInfo)
+	if err != nil {
+		log.Errorf("Json marshal %s error %v", containerName, err)
+		return
+	}
+	dirURL := fmt.Sprintf(container.DefaultInfoLocation, containerName)
+	configFilePath := dirURL + container.ConfigName
+	if err := os.WriteFile(configFilePath, newContentBytes, 0622); err != nil {
+		log.Errorf("Write file %s error", configFilePath, err)
+	}
+}
+
+func getContainerInfoByName(containerName string) (*container.Info, error) {
+	dirURL := fmt.Sprintf(container.DefaultInfoLocation, containerName)
+	configFilePath := dirURL + container.ConfigName
+	contentBytes, err := os.ReadFile(configFilePath)
+	if err != nil {
+		log.Errorf("Read file %s error %v", configFilePath, err)
+		return nil, err
+	}
+	var containerInfo container.Info
+	if err := json.Unmarshal(contentBytes, &containerInfo); err != nil {
+		log.Errorf("GetContainerInfoByName unmarshal error %v", err)
+		return nil, err
+	}
+	return &containerInfo, nil
+}
